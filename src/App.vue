c8cee645e19260443eb2802eb957d6c183584118
Bhubon Sutradhar Nil <54433969+bhubon@users.noreply.github.com>
2023-07-18 19:53:05 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+const imageUrl  = 'https://images.pexels.com/photos/16790135/pexels-photo-16790135/free-photo-of-fog-over-bare-trees-in-forest.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+const linkUrl  = 'https://google.com';
+</script>
+
+<template>
+
+  <img :src="imageUrl" />
+  <a :href="linkUrl">Click Me</a>
+
+</template>
+
+<style scoped>
+</style>
